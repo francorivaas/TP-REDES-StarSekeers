d31d4c9c62b6ad83412ba87e06ffcfd370989c9a
francorivaas <francoagustinrivas@hotmail.com>
2024-10-27 13:09:58 -0300

    adjustements

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Health playerHealth = collision.gameObject.GetComponent<Health>();
+
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
