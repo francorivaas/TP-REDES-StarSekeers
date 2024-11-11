68c25a8c54a677f6a551b5c86bec6daf9bcb7f7b
Federico Degraf <fedegraf98@gmail.com>
2024-11-11 11:15:07 -0300

    Messages of Error
    Messages of error when Player 1 is waiting for second player to join for more than a minute; and when there is an error trying to join a room

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -1,11 +1,17 @@
+using System;
 using Photon.Pun;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerSpawn : MonoBehaviour
 {
     public GameObject playerPrefab;
     private GameObject player;
     private PhotonView pv;
+    private bool HasToCheckForSecondPlayer;
+    [SerializeField] private float TimeToConnect = 60f;
+    private float CurrentTimeWaiting = 0f;
+    [SerializeField] private GameObject ErrorPopup;
 
     private void Awake()
     {
@@ -18,11 +24,38 @@ public class PlayerSpawn : MonoBehaviour
             Random.Range(-4, 4)), Quaternion.identity);
         int playerIndex = PhotonNetwork.PlayerList.Length;
         pv.RPC("ChangeColor", RpcTarget.AllBuffered, player.GetComponent<PhotonView>().ViewID, playerIndex);
+        if (PhotonNetwork.PlayerList.Length == 1)
+        {
+            HasToCheckForSecondPlayer = true;
+        }
+        
     }
     [PunRPC]
     private void ChangeColor(int playerViewID, int playerIndex)
     {
         PhotonView targetPhotonView = PhotonView.Find(playerViewID);
         if (targetPhotonView != null) targetPhotonView.gameObject.GetComponentInChildren<SpriteRenderer>().color = (playerIndex == 1) ? Color.blue : Color.red;
+    }
+
+    private void Update()
+    {
+        if (HasToCheckForSecondPlayer)
+        {
+            if (PhotonNetwork.PlayerList.Length == 2)
+            {
+                HasToCheckForSecondPlayer = false;
+                CurrentTimeWaiting = 0;
+            }
+            else
+            {
+                CurrentTimeWaiting += Time.deltaTime;
+                if (CurrentTimeWaiting >= TimeToConnect)
+                {
+                    ErrorPopup.SetActive(true);
+                    Time.timeScale = 0;
+                }
+   
+            }
+        }
     }
 }
